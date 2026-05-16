b4b70915368237a4e6b1b6ef6b5f7ff7920887a8
Artemis <74236653+happylittlejester@users.noreply.github.com>
2026-05-16 20:24:39 +0200

    Add logic.py test

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,4 +1,5 @@
 from app.pathfinding import astar_graph
+from app.logic import determine_result
 
 # pathfinding.py
 
@@ -32,4 +33,31 @@ def test_astar():
 
     # Path length
     assert len(result.path) >= 2
+
+# logic.py
+
+def test_out_at_first():
+    base = {"1B": 4, "2B": 8, "3B": 12, "HOME": 16}
+    field = {"1B": 3, "2B": 7, "3B": 11, "HOME": 15}
+    assert determine_result(base, field) == "OUT AT FIRST"
+
+def test_single():
+    base = {"1B": 3, "2B": 10, "3B": 20, "HOME": 30}
+    field = {"1B": 4, "2B": 5, "3B": 6, "HOME": 7}
+    assert determine_result(base, field) == "SINGLE"
+
+def test_double():
+    base = {"1B": 3, "2B": 6, "3B": 9, "HOME": 12}
+    field = {"1B": 4, "2B": 7, "3B": 5, "HOME": 13}
+    assert determine_result(base, field) == "DOUBLE"
+
+def test_triple():
+    base =  {"1B": 3, "2B": 6, "3B": 9, "HOME": 12}
+    field = {"1B": 4, "2B": 7, "3B": 10, "HOME": 8}
+    assert determine_result(base, field) == "TRIPLE"
+
+def test_inside_the_park_home_run():
+    base =  {"1B": 3, "2B": 6, "3B": 9, "HOME": 12}
+    field = {"1B": 5, "2B": 8, "3B": 11, "HOME": 20}
+    assert determine_result(base, field) == "INSIDE THE PARK HOME RUN"
 
